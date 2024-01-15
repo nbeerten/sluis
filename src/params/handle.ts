dd4d3f1f2a8affc58c9e2a417b0564fd030f8475
nbeerten <59444993+nbeerten@users.noreply.github.com>
2024-01-15 15:27:46 +0100

    Error page

---
+++ src/params/handle.ts
@@ -0,0 +1,5 @@
+import type { ParamMatcher } from "@sveltejs/kit";
+
+export const match: ParamMatcher = (param) => {
+    return /^@[A-Za-z0-9_\-.]{3,30}$/.test(param);
+}
---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<main>
+    <hgroup>
+        <h1 class="text-3xl font-bold">Error {$page.status}</h1>
+        <p class="text-sm text-muted-foreground">{$page.error?.message}</p>
+        <p>Try switching to another instance in the left sidebar</p>
+    </hgroup>
+</main>
