302d22bd036485c2d87d5f6c6c2c041963e29c79
nbeerten <59444993+nbeerten@users.noreply.github.com>
2024-01-20 13:50:29 +0100

    Fix searchbar issue

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,10 +1,11 @@
 import { error } from "@sveltejs/kit";
 
 export const load = async ({ fetch, url, locals }) => {
-    const query = url.searchParams.get("q");
+    let query = url.searchParams.get("q");
     if (!query) {
         error(404, { message: "Please provide a search query" });
     }
+    query = decodeURIComponent(query).trim();
     const filter = url.searchParams.get("filter") ?? "videos";
 
     if (!filter || ["videos", "channels", "playlists"].includes(filter ?? "") === false) {
